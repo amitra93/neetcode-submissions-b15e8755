3f1f0c145835f7a05d07fe7556d55ba08dc3a6ca
Anarghya Mitra <amitra93@gmail.com>
2026-04-08 19:38:53 -0400

    Add: design-twitter-feed - submission-2

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-2.py
@@ -0,0 +1,37 @@
+from collections import defaultdict
+from time import time
+
+class Twitter:
+
+    def __init__(self):
+        self.userToTweetMap = defaultdict(list)
+        self.userToFollowerMap = defaultdict(set)
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        self.userToTweetMap[userId].append((time(), tweetId))
+        print(self.userToTweetMap)
+
+    def getNewsFeed(self, userId: int) -> List[int]:
+        tweets = []
+        for tweet in self.userToTweetMap[userId]:
+            print(tweet)
+            tweets.append(tweet)
+        followers = self.userToFollowerMap[userId]
+        for follower in followers:
+            for tweet in self.userToTweetMap[follower]:
+                print(tweet)
+                tweets.append(tweet)
+        print(tweets)
+        sortedTweets = [tweet[1] for tweet in sorted(tweets, reverse = True)]
+        if len(sortedTweets) <= 10:
+            return sortedTweets
+        return sortedTweets[0:10]
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        if followerId != followeeId:
+            self.userToFollowerMap[followerId].add(followeeId)
+        print(self.userToFollowerMap)
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        self.userToFollowerMap[followerId].discard(followeeId)
+        print(self.userToFollowerMap)
