0f9f163941aa51e7e36ba1a1bbbe2154baa0f463
Anarghya Mitra <amitra93@gmail.com>
2026-04-14 15:24:09 -0400

    Add: lru-cache - submission-3

---
+++ Data Structures & Algorithms/lru-cache/submission-3.py
@@ -0,0 +1,72 @@
+class DLNode:
+    def __init__(self):
+        self.key = None
+        self.val = None
+        self.next = None
+        self.prev = None
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cache = {}
+        self.capacity = capacity
+        self.lruSentinel = DLNode()
+        self.lruSentinel.key = -1
+        self.mruSentinel = DLNode()
+        self.mruSentinel.key = 1001
+        self.lruSentinel.next = self.mruSentinel
+        self.mruSentinel.prev = self.lruSentinel
+
+        cur = self.lruSentinel
+        nodesList = list()
+        while cur.next is not None:
+            nodesList.append(str(cur.key))
+            cur = cur.next
+        print(",".join(nodesList))
+
+    def _append_to_mru(self, node: DLNode):
+        oldNode = self.mruSentinel.prev
+        oldNode.next = node
+        node.next = self.mruSentinel
+        self.mruSentinel.prev = node
+        node.prev = oldNode
+
+    def _move_to_mru(self, node: DLNode):
+        leftNode = node.prev
+        rightNode = node.next
+        leftNode.next = rightNode
+        rightNode.prev = leftNode
+        self._append_to_mru(node)
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        node = self.cache[key]
+        if node.next != self.mruSentinel:
+            self._move_to_mru(node)
+        return node.val
+
+    def put(self, key: int, value: int) -> None:
+        print(f"putting {key}")
+        if key in self.cache:
+            node = self.cache[key]
+            node.val = value
+            self._move_to_mru(node)
+        else:
+            if len(self.cache) >= self.capacity:
+                del self.cache[self.lruSentinel.next.key]
+                nodeToSkipTo = self.lruSentinel.next.next
+                self.lruSentinel.next = nodeToSkipTo
+                nodeToSkipTo.prev = self.lruSentinel
+            node = DLNode()
+            node.key = key
+            node.val = value
+            self.cache[key] = node
+            self._append_to_mru(node)
+        cur = self.lruSentinel
+        nodesList = list()
+        while cur.next is not None:
+            nodesList.append(str(cur.key))
+            cur = cur.next
+        print(",".join(nodesList))
+                
