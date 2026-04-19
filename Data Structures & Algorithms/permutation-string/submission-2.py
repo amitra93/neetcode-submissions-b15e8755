1f9dd25fb5b03ae4b477dd3024001a03907c1c58
Anarghya Mitra <amitra93@gmail.com>
2026-04-18 20:05:47 -0400

    Add: permutation-string - submission-2

---
+++ Data Structures & Algorithms/permutation-string/submission-2.py
@@ -0,0 +1,25 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+
+        def dictify(s: str) -> dict:
+            sdict = {}
+            for i in s:
+                sdict[i] = sdict.get(i, 0) + 1
+            return sdict
+
+        ctr = len(s1)
+        expected = dictify(s1)
+        actual = dictify(s2[0: ctr])
+        while ctr < len(s2):
+            print(f"begin loop, expected = {expected}, actual = {actual}, ctr={ctr}")
+            if expected == actual:
+                return True
+            actual[s2[ctr - len(s1)]] -= 1
+            if actual[s2[ctr - len(s1)]] == 0:
+                del actual[s2[ctr - len(s1)]]
+            actual[s2[ctr]] = actual.get(s2[ctr], 0) + 1
+            ctr += 1
+            print(f"after loop, expected = {expected}, actual = {actual}, ctr={ctr}")
+        return expected == actual
