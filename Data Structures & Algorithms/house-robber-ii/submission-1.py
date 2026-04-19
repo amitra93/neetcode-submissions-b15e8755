d70c80ab5285aa20506d9c5e05c680da3fafd004
Anarghya Mitra <amitra93@gmail.com>
2026-04-19 14:07:12 -0400

    Add: house-robber-ii - submission-1

---
+++ Data Structures & Algorithms/house-robber-ii/submission-1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) < 3:
+            return max(nums)
+
+        def houseRob1(numsSubset: List[int]) -> int:
+            maxRob = [0] * len(numsSubset)
+            maxRob[0] = numsSubset[0]
+            maxRob[1] = max(numsSubset[1], maxRob[0])
+            for i in range(2, len(numsSubset)):
+                maxRob[i] = max(numsSubset[i] + maxRob[i-2], maxRob[i-1])
+            return maxRob[-1]
+
+        return max(houseRob1(nums[0: -1]), houseRob1(nums[1:]))
