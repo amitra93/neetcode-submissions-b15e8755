acbc2619d3825109f7cbeca093cea475133e2d06
Anarghya Mitra <amitra93@gmail.com>
2026-04-20 19:15:29 -0400

    Add: count-connected-components - submission-2

---
+++ Data Structures & Algorithms/count-connected-components/submission-2.py
@@ -0,0 +1,31 @@
+import collections
+
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        adjList = collections.defaultdict(list)
+        for edge in edges:
+            adjList[edge[0]].append(edge[1])
+            adjList[edge[1]].append(edge[0])
+        
+        visited = set()
+        components = 0
+
+        def findFirstNotVisited():
+            for node in adjList:
+                if node not in visited:
+                    return node
+            raise ValueError
+
+        while len(visited) != len(adjList):
+            unvisitedNode = findFirstNotVisited()
+            queue = collections.deque([unvisitedNode])
+            while len(queue) > 0:
+                node = queue.popleft()
+                if node in visited:
+                    continue
+                visited.add(node)
+                for neighbor in adjList[node]:
+                    queue.append(neighbor)
+            components += 1
+        return components + (n - len(visited))
+
