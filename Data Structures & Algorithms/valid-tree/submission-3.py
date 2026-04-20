674a58521812bcd49d18c65d2610e7b2c0470dc8
Anarghya Mitra <amitra93@gmail.com>
2026-04-20 17:57:27 -0400

    Add: valid-tree - submission-3

---
+++ Data Structures & Algorithms/valid-tree/submission-3.py
@@ -0,0 +1,28 @@
+import collections
+
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        if len(edges) == 0:
+            return True
+        if len(edges) == 2:
+            return False
+
+        adjList = collections.defaultdict(list)
+        for edge in edges:
+            adjList[edge[0]].append(edge[1])
+            adjList[edge[1]].append(edge[0])
+
+        queue = collections.deque([(-1, 1)])
+        visited = set()
+        while len(queue) > 0:
+            prev, cur = queue.popleft()
+            print(f"visited: {visited}, prev: {prev}, cur: {cur}")
+            if cur in visited:
+                return False
+            visited.add(cur)
+            for i in adjList[cur]:
+                if i == prev:
+                    continue
+                queue.append((cur, i))
+        return len(visited) == len(adjList)
+
